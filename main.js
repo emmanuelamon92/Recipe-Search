88bc9e3ae8cda30df52532152b700a021db46ad8
Emmanuel Amon <emmanuelamon@Emmanuels-MacBook-Pro.local>
2017-08-12 13:54:49 -0400

    itunes project made this simplier' :

--- main.js
+++ main.js
@@ -2,40 +2,63 @@
 //section => div =>
 //loop to get each box from the api search
 //form!!!!??
+let lowerLevelBox = document.querySelector(".lowerLevel");
+let userSearchBox = document.querySelector(".userSearch")
 
-let url = "https://proxy.calweb.xyz/http://www.recipepuppy.com/api/?i=pizza";
+let url = "https://proxy.calweb.xyz/http://www.recipepuppy.com/api/?i=";
 
+userSearchBox.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let query = event.target.querySelector("input[name='userInput']").value;
+  query = encodeURIComponent(query);
 
-
-fetch(url)
+fetch(url + query)
   .then(function(response) {
     response.json().then(function(data) {
-      console.log("my data", data);
-      let lowerLevelBox = document.querySelector(".lowerLevel");
+      let recipes = data.results;
+      console.log(recipes);
+      // console.log("my data", data);
 
 
-      let foodResult = []
+      // let foodResult = []
 
-      function ingredientBox() {
-        for (var i = 0; i < data.results.length; i++) {
-          foodResult.push(data.results[i].title);
-          foodResult.push(data.results[i].href);
-          foodResult.push(data.results[i].thumbnail);
-        }
-        const boxOfIngredients = `
+
+        for (var i = 0; i < recipes.length; i++) {
+
+          let recipe = recipes[i];
+          // function ingredientBox(recipe) {
+          // let boxOfIngredients =
+
+          // console.log("boxOfIngredients", boxOfIngredients);
+          // return boxOfIngredients;
+
+          // let findLowerLevelBox = ingredientBox(recipe);
+          lowerLevelBox.innerHTML +=
+                        `
                         <div class = "myBox">
-                            <p>${"foodResult"}</p>
+                            <p>${recipe.title}</p>
+                            <p>${recipe.href}</p>
+                            <p>${recipe.thumbnail}</p>
                         </div>
-                      `
-              console.log("boxOfIngredients", boxOfIngredients);
-      };
-      console.log("data", data);
-      console.log("foodResult", foodResult);
-      let findlowerLevelBox = ingredientBox(data);
-      lowerLevelBox.innerHTML = findlowerLevelBox;
+                          `
+          // recipe = 0;
+          // console.log(lowerLevelBox);
+        }
+        recipe = 0;
+        // query = 0;
+        // userSearchBox = 0;
+        // lowerLevelBox = 0;
+        // console.log("boxOfIngredients2", boxOfIngredients);
+      //   let findLowerLevelBox = ingredientBox(recipe);
+      //   lowerLevelBox.innerHTML = findLowerLevelBox;
+      //   console.log(findLowerLevelBox);
+      // };
+      // console.log("data", data);
+      // console.log("foodResult", foodResult);
+
     });
   });
-
+});
 
 //search event for <input> type search event
 //searchINput.addEventListener('search', function(event)){
